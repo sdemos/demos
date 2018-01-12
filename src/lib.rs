cb70b5dcbbe0466f26a492fce6f679e46f5f2f8a
Stephen Demos <stphndemos@gmail.com>
2018-01-11 22:09:16 -0800

    lib: more interesting rust program
    it prints hello world!

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,23 @@
 #![feature(lang_items)]
 #![no_std]
 
+extern crate rlibc;
+
 #[no_mangle]
-pub extern fn rust_main() {}
+pub extern fn rust_main() {
+    let hello = b"Hello World!";
+    let color_byte = 0x1f;
+
+    let mut hello_colored = [color_byte; 24];
+    for (i, char_byte) in hello.into_iter().enumerate() {
+        hello_colored[i*2] = *char_byte;
+    }
+
+    let buffer_ptr = (0xb8000 + 1988) as *mut _;
+    unsafe { *buffer_ptr = hello_colored };
+
+    loop {}
+}
 
 #[lang = "eh_personality"]
 #[no_mangle]
