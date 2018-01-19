07ec3bd6b005fa04ad214392f851afde873efcfb
Stephen Demos <stphndemos@gmail.com>
2018-01-18 17:40:34 -0800

    memory: add FrameIter iterator

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -9,6 +9,11 @@ use self::paging::PhysicalAddress;
 
 pub const PAGE_SIZE: usize = 4096;
 
+pub trait FrameAllocator {
+    fn allocate_frame(&mut self) -> Option<Frame>;
+    fn deallocate_frame(&mut self, frame: Frame);
+}
+
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Frame {
     number: usize,
@@ -33,9 +38,30 @@ impl Frame {
     fn clone(&self) -> Frame {
         Frame { number: self.number }
     }
+
+    fn range_inclusive(start: Frame, end: Frame) -> FrameIter {
+        FrameIter {
+            start: start,
+            end: end,
+        }
+    }
 }
 
-pub trait FrameAllocator {
-    fn allocate_frame(&mut self) -> Option<Frame>;
-    fn deallocate_frame(&mut self, frame: Frame);
+struct FrameIter {
+    start: Frame,
+    end: Frame,
+}
+
+impl Iterator for FrameIter {
+    type Item = Frame;
+
+    fn next(&mut self) -> Option<Frame> {
+        if self.start <= self.end {
+            let frame = self.start.clone();
+            self.start.number += 1;
+            Some(frame)
+        } else {
+            None
+        }
+    }
 }
